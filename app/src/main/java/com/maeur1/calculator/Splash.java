d280ba6ea1e91a72f7e860c3af6b300e747ee9a1
Mayur <panchal.mayur2@gmail.com>
2014-04-20 21:57:13 +1200

    Worked on it a bit
    Now it works atleast

--- app/src/main/java/com/maeur1/calculator/Splash.java
+++ app/src/main/java/com/maeur1/calculator/Splash.java
@@ -1,6 +1,7 @@
 package com.maeur1.calculator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends Activity{
@@ -9,5 +10,18 @@ public class Splash extends Activity{
     protected void onCreate(Bundle ThisIsBacon) {
         super.onCreate(ThisIsBacon);
         setContentView(R.layout.splash);
+        Thread timer = new Thread(){
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally{
+                    Intent openStartingPoint = new Intent("android.intent.action.MAINACTIVITY");
+                    startActivity(openStartingPoint);
+                }
+            }
+        };
+        timer.start();
     }
 }
