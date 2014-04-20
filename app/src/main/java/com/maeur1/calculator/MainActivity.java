0d8cd55d8349cb08e96b2628f3b3fabcc914b988
Mayur <panchal.mayur2@gmail.com>
2014-04-20 16:21:47 +1200

    Initial Commit

---
+++ app/src/main/java/com/maeur1/calculator/MainActivity.java
@@ -0,0 +1,33 @@
+package com.maeur1.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    int counter;
+    Button add, sub;
+    TextView display;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        counter = 0;
+        add = (Button) findViewById(R.id.bAdd);
+        sub = (Button) findViewById(R.id.bSub);
+        display = (TextView) findViewById(R.id.tvDisplay);
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                counter++;
+                display.setText("Your total is " + counter);
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                counter--;
+                display.setText("Your total is " + counter);
+            }
+        });
+    }
+}
