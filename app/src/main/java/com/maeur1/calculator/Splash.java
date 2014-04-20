d970dbe725290cbb5a3a94cda6c19ecb808bc6b0
Mayur <panchal.mayur2@gmail.com>
2014-04-20 21:40:26 +1200

    Worked on it a bit
    Now it works atleast

---
+++ app/src/main/java/com/maeur1/calculator/Splash.java
@@ -0,0 +1,13 @@
+package com.maeur1.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity{
+
+    @Override
+    protected void onCreate(Bundle ThisIsBacon) {
+        super.onCreate(ThisIsBacon);
+        setContentView(R.layout.splash);
+    }
+}
