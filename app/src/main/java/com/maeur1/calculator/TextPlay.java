535e04cd82455616bf22274f5210073e17ba2f06
Mayur <panchal.mayur2@gmail.com>
2014-04-21 14:51:51 +1200

    The Fridge was found. Ignore where the fridge is.

--- app/src/main/java/com/maeur1/calculator/TextPlay.java
+++ app/src/main/java/com/maeur1/calculator/TextPlay.java
@@ -1,11 +1,17 @@
 package com.maeur1.calculator;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import java.util.Random;
 
 /**
  * Created by Mayur on 21/04/14.
@@ -16,9 +22,44 @@ public class TextPlay extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.text);
 
-        Button checkCmd = (Button) findViewById(R.id.bResults);
-        ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
-        EditText input = (EditText) findViewById(R.id.etCommands);
-        TextView display = (TextView) findViewById(R.id.tvResults);
+        Button chkCmd = (Button) findViewById(R.id.bResults);
+        final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
+        final EditText input = (EditText) findViewById(R.id.etCommands);
+        final TextView display = (TextView) findViewById(R.id.tvResults);
+
+        passTog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (passTog.isChecked()) {
+                    input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                }
+            }
+        });
+        chkCmd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String check = input.getText().toString();
+                display.setText(check);
+                if (check.contentEquals("left")) {
+                    display.setGravity(Gravity.LEFT);
+                } else if (check.contentEquals("center")) {
+                    display.setGravity(Gravity.CENTER);
+                } else if (check.contentEquals("right")) {
+                    display.setGravity(Gravity.RIGHT);
+                } else if (check.contentEquals("blue")) {
+                    display.setTextColor(Color.BLUE);
+                } else if (check.contentEquals("WTF")) {
+                    Random crazy = new Random();
+                    display.setText("WTF!!!!");
+                    display.setTextSize(crazy.nextInt(75));
+                    display.setTextColor(Color.rgb(crazy.nextInt(255), crazy.nextInt(255), crazy.nextInt(255)));
+                } else {
+                    display.setText("invalid");
+                    display.setGravity(Gravity.CENTER);
+                }
+            }
+        });
     }
 }
